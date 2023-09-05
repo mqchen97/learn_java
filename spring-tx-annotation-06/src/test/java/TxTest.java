c309d97034beed5c18301e77aa4c91a33a86f981
chenmengqing <chenmq521@gmail.com>
2023-09-05 13:45:21 +0800

    feat(CI): xx

--- spring-tx-annotation-06/src/test/java/TxTest.java
+++ spring-tx-annotation-06/src/test/java/TxTest.java
@@ -1,17 +1,19 @@
 import com.atguigu.config.JavaConfig;
-import com.atguigu.controller.StudentController;
+import com.atguigu.service.StudentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.io.FileNotFoundException;
 
 @SpringJUnitConfig(JavaConfig.class)
 public class TxTest {
 
     @Autowired
-    private StudentController studentController;
+    private StudentService studentService;
 
     @Test
-    public void testTx(){
-        studentController.changeInfo();
+    public void testTx() throws FileNotFoundException {
+        studentService.changeInfo();
     }
 }
